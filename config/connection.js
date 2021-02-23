f3d204377af9d2bfee7f1000298d8b495117dffd
binm <binm@amazon.com>
2021-02-22 19:06:02 -0800

    change view files structure

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 // Set up MySQL connection.
+require('dotenv').config();
 const mysql = require('mysql');
 
 const connection = process.env.JAWSDB_URL? mysql.createConnection("mysql://xdj9qvu54tmf9e2z:jyw0li0dgagaop7n@jhdjjtqo9w5bzq2t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/izziao18bj4eluur"):
@@ -7,7 +8,7 @@ mysql.createConnection({
   port: 3306,
   user: 'root',
   // NOTE: Be sure to add your MySQL password here!
-  password: '',
+  password: process.env.DB_PASSWORD,
   database: 'cat_db',
 });
 
